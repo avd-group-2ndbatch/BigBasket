100a8f0f9dc780f989c8b7f621a171694094e704
Bishwajeet Mahanta <biswajit.kjr55@gmail.com>
2023-07-02 04:49:00 +0000

    This is 11th commit

--- evenodd.sh
+++ evenodd.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
+This commit is 11th.
 
 # This program is for checking a given number is an even or odd number
+
+ (All files comitting to demonstrate git config is working or not)
 
 NUM=$1
 if [ `expr $NUM % 2`-eq 0 ]
