8884dd257687d1ff7b7789b51aa455763cff67f5
bishwajeet0771 <69516107+bishwajeet0771@users.noreply.github.com>
2023-07-04 10:11:31 +0530

    Update pallindrome.sh
    My friends name added to pallindrome

--- pallindrome.sh
+++ pallindrome.sh
@@ -3,3 +3,7 @@
 This is a program which is going to compare two words
 if they are same, then the word is a pallindrome
 I like to write a code which is going to check whether a number is a pallindrome or not
+Rahul
+Meena
+Pinku
+Rinku
